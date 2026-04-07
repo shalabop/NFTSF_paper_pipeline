01d5816eabe29c8a15af96c8df68d799fc147702
[ Mahmoud Ahmed - Undergraduate - Ira A Fulton Engineering - Undergraduate Student ] <mahmuodimad@gmail.com>
2026-04-06 19:26:30 -0700

    added DATA

--- preprocessing/normalize.py
+++ preprocessing/normalize.py
@@ -29,16 +29,18 @@ from pathlib import Path
 def normalize_all(prediction_length=100, context_length=100, train_test_split=900, train_val_split=900):
     ##must be named exactly
     datasets = ["double_well","single_well", "alanine_phi", "alanine_psi", "linear_gaussian"]
-    out_dir = Path("DATA") ##change this if you need the folder to be named something else
+    out_dir = Path("DATA_unnorm") ##change this if you need the folder to be named something else
     out_dir.mkdir(exist_ok=True)
     
 
     for ds in datasets:
-        print(f"\nProcessing {ds}...")
+        print(f"\n=================================================================")
+        print(f"Processing {ds}")
+        print(f"=================================================================")
 
         #DATA_unnorm where is the raw unnormalized is 
-        train_val_raw = Path(f"DATA_unnorm/{ds}_train.npy")
-        test_raw = Path(f"DATA_unnorm/{ds}_test.npy")
+        train_val_raw = Path(f"DATA_raw/{ds}_train.npy")
+        test_raw = Path(f"DATA_raw/{ds}_test.npy")
 
         if not train_val_raw.exists() or not test_raw.exists():
             print(f"  Missing raw files for {ds}, skipping.")
@@ -85,6 +87,36 @@ def normalize_all(prediction_length=100, context_length=100, train_test_split=90
         print(f'Validation set: {val_data.shape}')
         
         
+        print("==="*15)
+        print("Calculating mean and std from training set only")
+        print("==="*15)
+        
+        mean = np.mean(train_data)
+        std = np.std(train_data)
+        print(f"Mean: {mean}, Std: {std}")
+        
+        time = np.arange(train_val_data_transposed.shape[1]) ##time is the same for train and val, and test
+        
+        ##for training all methods except tsdiff/tsdiff_cond, we only need the train_val.npz file, which includes the training and validation data normalized by the training set statistics. 
+        # For tsdiff/tsdiff_cond, we will also create the gluonts_datasets with the same normalized data.
+        np.savez(out_dir/f"{ds}_train.npz", positions=train_val_data_transposed, train_val_split=train_val_split, time=time, prediction_length=prediction_length, context_length=context_length, mean=mean, std=std) 
+        np.savez(out_dir/f"{ds}_test.npz", positions=test_data_transposed, train_test_split=train_test_split, time=time, prediction_length=prediction_length, context_length=context_length, mean=mean, std=std)
+        np.savez(out_dir/f"{ds}.npz", positions_train=train_val_data_transposed, positions_test=test_data_transposed, train_test_split=train_test_split, time=time, prediction_length=prediction_length, context_length=context_length, mean=mean, std=std) ##for tsdiff/tsdiff_cond pipeline only
+        
+        ##create normlaized data using stats from training set only
+        train_val_data_normalized = (train_val_data_transposed - mean) / (std + 1e-8)
+        test_data_normalized = (test_data_transposed - mean) / (std + 1e-8)
+        
+        ##save the normalized data in the same format as above for tsdiff/tsdiff_cond pipeline only
+        out_dir_norm=Path("DATA") ##change this if you need the folder to be named something else
+        out_dir_norm.mkdir(exist_ok=True)
+        
+        
+        np.savez(out_dir_norm/f"{ds}_train.npz", positions=train_val_data_normalized, train_val_split=train_val_split, time=time, prediction_length=prediction_length, context_length=context_length, mean=mean, std=std)
+        np.savez(out_dir_norm/f"{ds}_test.npz", positions=test_data_normalized, train_test_split=train_test_split, time=time, prediction_length=prediction_length, context_length=context_length, mean=mean, std=std)
+        np.savez(out_dir_norm/f"{ds}.npz", positions_train=train_val_data_normalized, positions_test=test_data_normalized, train_test_split=train_test_split, time=time, prediction_length=prediction_length, context_length=context_length, mean=mean, std=std) ##for tsdiff/tsdiff_cond pipeline only
+
+
 
 if __name__ == '__main__':
     normalize_all()
